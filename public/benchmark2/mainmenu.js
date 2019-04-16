0730fc3b44ee25077052b7cfa91bb4e39b88ee0b
Rahul Sondhi <hungryhul@gmail.com>
2019-04-15 20:37:28 -0400

    New sprites

--- public/benchmark2/mainmenu.js
+++ public/benchmark2/mainmenu.js
@@ -1,17 +1,28 @@
 var textStyle = {
-    font: "normal 20px Arial",
+    font: "normal 14px Arial",
     fill: '#ffffff',
-    align: 'center',
-    boundsAlignH: "center", // bounds center align horizontally
-    boundsAlignV: "middle" // bounds center align vertically
+    align: 'left',
+    wordWrap:{
+      width: 300, useAdvancedWrap: true
+    }
 };
 
 var headerStyle = {
-    font: "normal 30px Arial",
+    font: "normal 18px Arial",
     fill: '#ffffff',
     align: 'center',
-    boundsAlignH: "center", // bounds center align horizontally
-    boundsAlignV: "middle" // bounds center align vertically
+    wordWrap:{
+      width: 300, useAdvancedWrap: true
+    }
+};
+
+var titleStyle = {
+    font: "normal 24px Arial",
+    fill: '#ffffff',
+    align: 'center',
+    wordWrap:{
+      width: 300, useAdvancedWrap: true
+    }
 };
 
 var MainMenu = new Phaser.Class({
@@ -32,6 +43,7 @@ var MainMenu = new Phaser.Class({
     this.load.image("tiles", "assets/tileset/custtiles1.png");
     this.load.image("logo", "assets/branding/logo.png");
     this.load.tilemapTiledJSON('map', 'assets/tilemap/level1map.json');
+    this.load.atlas('buttons', 'assets/buttons/buttons.png', 'assets/buttons/buttons.json');
   },
 
   create: function() {
@@ -44,13 +56,14 @@ var MainMenu = new Phaser.Class({
     worldLayer = map.createDynamicLayer("BackLayer", tileset, 0, 0);
 
     var logo = this.add.sprite(400, 140, 'logo');
-    this.newgame = this.addButton(400, 260, 'sprites', this.doStart, this, 'btn_quest_hl', 'btn_quest', 'btn_quest_hl', 'btn_quest');
-    this.levelsel = this.addButton(320, 320, 'sprites', this.doLevel, this, 'btn_play_hl', 'btn_play', 'btn_play_hl', 'btn_play');
-    this.controls = this.addButton(320, 380, 'sprites', this.doControls, this, 'btn_quest_hl', 'btn_quest', 'btn_quest_hl', 'btn_quest');
-    this.settings = this.addButton(480, 320, 'sprites', this.doSettings, this, 'btn_play_hl', 'btn_play', 'btn_play_hl', 'btn_play');
-    this.help = this.addButton(480, 380, 'sprites', this.doHelp, this, 'btn_quest_hl', 'btn_quest', 'btn_quest_hl', 'btn_quest');
+    this.add.image(160, 100, 'buttons', 'helpButtonHover');
+    this.newgame = this.addButton(400, 260, 'buttons', this.doStart, this, 'btn_quest_hl', 'btn_quest', 'btn_quest_hl', 'btn_quest');
+    this.levelsel = this.addButton(320, 320, 'buttons', this.doLevel, this, 'btn_play_hl', 'btn_play', 'btn_play_hl', 'btn_play');
+    this.controls = this.addButton(320, 380, 'buttons', this.doControls, this, 'controlsButtonHover', 'controlsButton', 'controlsButtonHover', 'controlsButton');
+    this.settings = this.addButton(480, 320, 'buttons', this.doSettings, this, 'btn_play_hl', 'btn_play', 'btn_play_hl', 'btn_play');
+    this.help = this.addButton(480, 380, 'buttons', this.doHelp, this, 'helpButtonHover', 'helpButton', 'helpButtonHover', 'help');
 
-    console.log('create is ready');
+    console.log(this);
   },
 
   doStart: function() {
@@ -145,6 +158,20 @@ var ControlsMenu = new Phaser.Class({
     worldLayer = map.createDynamicLayer("BackLayer", tileset, 0, 0);
     var logo = this.add.sprite(400, 140, 'logo');
 
+    var controls = [
+      "Move Left = Left key",
+      "",
+      "Move Right = Right key",
+      "",
+      "Aiming = Mouse",
+      "",
+      "Shoot = Left Click",
+      "",
+      "Weapon swap = Right Click"
+    ]
+
+    this.add.text(275,200,controls,headerStyle);
+
     // add tutorial and start button
     this.exit = this.addButton(400, 420, 'sprites', this.doReturn, this, 'btn_quest_hl', 'btn_quest', 'btn_quest_hl', 'btn_quest');
   },
@@ -228,10 +255,7 @@ var HelpMenu = new Phaser.Class({
     worldLayer = map.createDynamicLayer("BackLayer", tileset, 0, 0);
     var logo = this.add.sprite(400, 140, 'logo');
 
-    var background = [
-      "Game Developed By:",
-      "Immanuel Almosara, Sebastian Chamorro, Rahul Sondhi"
-    ]
+    var madeBy = "Immanuel Almosara, Sebastian Chamorro, Rahul Sondhi"
 
     var story = "Return to Sender is a game where our main character fights his way through all of the delivery drones that are hovering over his house. The delivery drones are sent by the well renowned company Amazon who felt they could be more efficent by automating their delivries as opposed to hiring workers. Our main character feels threatened by these machines and decides to shoot as many of them down as possible, and stealing all of the mail going to his neighbors."
 
@@ -244,6 +268,13 @@ var HelpMenu = new Phaser.Class({
       "",
       "The main character is a southern gun-loving obese male who isn't too familiar with advanced technology and firmly believes the world is better without it."
     ]
+
+    this.add.text(275,210,"Game Designed By",headerStyle);
+    this.add.text(275,230,madeBy,textStyle);
+    this.add.text(50,270,"Story",headerStyle);
+    this.add.text(50,290,story,textStyle);
+    this.add.text(500,270,"Characters",headerStyle);
+    this.add.text(500,290,story,textStyle);
 
     // add tutorial and start button
     this.exit = this.addButton(400, 420, 'sprites', this.doReturn, this, 'btn_quest_hl', 'btn_quest', 'btn_quest_hl', 'btn_quest');
